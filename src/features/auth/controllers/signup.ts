4a969519e1a791c47180e8a516cee8b0f5a2372e
riteshbenjwal <riteshbenjwal7@gmail.com>
2023-03-12 22:09:08 +0530

    user auth worker and user auth queue added for signup

--- src/features/auth/controllers/signup.ts
+++ src/features/auth/controllers/signup.ts
@@ -12,6 +12,11 @@ import { uploads } from '@global/helpers/cloudinary-upload';
 import HTTP_STATUS from 'http-status-codes';
 import { IUserDocument } from '@user/interfaces/user.interface';
 import { UserCache } from '@service/redis/user.cache';
+import { omit } from 'lodash';
+import { authQueue } from '@service/queues/auth.queue';
+import { userQueue } from '@service/queues/user.queue';
+import JWT from 'jsonwebtoken';
+import { config } from '@root/config';
 
 const userCache: UserCache = new UserCache();
 
@@ -59,7 +64,7 @@ export class SignUp {
       throw new BadRequestError('Error uploading image, Try again');
     }
 
-    // Add to redis cache
+    // // Add to redis cache
 
     const userDataForCache: IUserDocument = SignUp.prototype.userData(authData, userObjectId);
 
@@ -67,9 +72,42 @@ export class SignUp {
 
     await userCache.saveUserToCache(`${userObjectId}`, uId, userDataForCache);
 
-    res.status(HTTP_STATUS.CREATED).json({
-      message: 'User created successfully'
+    // Add to database
+
+    omit(userDataForCache, ['uId', 'username', 'email', 'avatarColor', 'password']);
+
+    authQueue.addAuthUserJob('addAuthUserToDB', {
+      value: userDataForCache
     });
+
+    userQueue.addUserJob('addUserToDB', {
+      value: userDataForCache
+    });
+
+    const userJwt: string = SignUp.prototype.signToken(authData, userObjectId);
+
+    req.session = {
+      jwt: userJwt
+    };
+
+    res.status(HTTP_STATUS.CREATED).json({
+      message: 'User created successfully',
+      user: userDataForCache,
+      token: userJwt
+    });
+  }
+
+  private signToken(data: IAuthDocument, userObjectId: ObjectId): string {
+    return JWT.sign(
+      {
+        userId: userObjectId,
+        uId: data.uId,
+        email: data.email,
+        username: data.username,
+        avatarColor: data.avatarColor
+      },
+      config.JWT_TOKEN!
+    );
   }
 
   private userData(data: IAuthDocument, userObjectId: ObjectId): IUserDocument {
--- src/features/auth/interfaces/auth.interface.ts
+++ src/features/auth/interfaces/auth.interface.ts
@@ -1,5 +1,6 @@
 import { Document } from 'mongoose';
 import { ObjectId } from 'mongodb';
+import { IUserDocument } from '@user/interfaces/user.interface';
 
 // To add property inside existing namespace
 declare global {
@@ -43,5 +44,5 @@ export interface ISignUpData {
 }
 
 export interface IAuthJob {
-  value?: string | IAuthDocument;
+  value?: string | IAuthDocument | IUserDocument;
 }
