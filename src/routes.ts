a11ea3490797da274190490718cf37e2e4f67ce4
riteshbenjwal <riteshbenjwal7@gmail.com>
2023-03-12 18:04:43 +0530

    user redis cache setup and signup flow

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,11 @@
 import { Application } from 'express';
+import { authRoutes } from '@auth/routes/authRoutes';
+
+const BASE_PATH = '/api/v1';
 
 export default (app: Application) => {
-  //eslint-disable-next-line @typescript-eslint/no-empty-function
-  const routes = () => {};
+  const routes = () => {
+    app.use(BASE_PATH, authRoutes.routes());
+  };
   routes();
 };
